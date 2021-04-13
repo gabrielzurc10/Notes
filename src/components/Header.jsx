61d1cf3b82bd074e11f563911043974e2e7b42c7
Gabriel Cruz <62117580+gabrielzurc10@users.noreply.github.com>
2021-04-12 21:59:29 -0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+      <i class="fas fa-feather-alt fa-2x"></i>
+        Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
